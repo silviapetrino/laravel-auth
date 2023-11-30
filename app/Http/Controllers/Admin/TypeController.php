165e979b1e0e2441f2ce2df1e95abac66c28e76d
silviapetrino <petrino.silvia@gmail.com>
2023-11-30 14:39:21 +0100

    [add] index to type and technology entities

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Type;
 
 class TypeController extends Controller
 {
@@ -14,7 +15,8 @@ class TypeController extends Controller
      */
     public function index()
     {
-        //
+        $types = Type::orderBy('id', 'desc')->get();
+        return view('admin.types.index', compact('types'));
     }
 
     /**
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -4,12 +4,30 @@
     {{-- contiene una nav della dashboard  --}}
 
     <nav>
-        <ul>
+        <ul class="pt-4">
             <li>
-                <a href="{{ route('admin.home')}}"><i class="fa-solid fa-house"></i></a>
+                <a href="{{ route('admin.home')}}">
+                    <i class="fa-solid fa-house"></i>
+                    <span>Dashboard</span>
+                </a>
             </li>
             <li>
-                <a href="{{ route('admin.projects.index')}}">Project list</a>
+                <a href="{{ route('admin.projects.index')}}">
+                    <i class="fa-solid fa-list"></i>
+                    <span>Project list</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.technologies.index')}}">
+                    <i class="fa-solid fa-gear"></i>
+                    <span>Technologies</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.types.index')}}">
+                    <i class="fa-regular fa-circle"></i>
+                    <span>Types</span>
+                </a>
             </li>
         </ul>
     </nav>
